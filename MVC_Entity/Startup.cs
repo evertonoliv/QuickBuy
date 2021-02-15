0e6fe6f4adb02d6e91b98de595d6a480d57964fe
Everton Oliveira <everton@admdeveloper.com.br>
2021-02-15 13:05:28 -0300

    Teste new Vecel

---
+++ MVC_Entity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Entity.Startup))]
+namespace MVC_Entity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
